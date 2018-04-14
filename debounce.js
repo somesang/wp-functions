80cec7917aaf8157297469f92445e378b08240ef
somesang <sanyoeun@gmail.com>
2018-04-14 17:27:30 -0400

    Initial commit

---
+++ debounce.js
@@ -0,0 +1,20 @@
+/*
+*
+ * Debounce function to reduce how many times to run a function
+ * ie. EventListeners to detect scrolling
+ *
+*/
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
